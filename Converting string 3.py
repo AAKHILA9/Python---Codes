4b00eeafe472d02b343ef2f0d113bbfecf920da7
A.Akhila <anemoniakhila123456@gmail.com>
2025-07-23 10:53:32 +0530

    Add files via upload

---
+++ Converting string 3.py
@@ -0,0 +1,16 @@
+string="I AM THE WINNER"
+reverse=string.split()
+for word in reversed(reverse):
+    reversed_word=word[::-1]
+    print(reversed_word,len(reversed_word))
+
+    #OR
+string="I AM THE WINNER"
+reverse=string[::-1]
+words=reverse.split()
+for word in words:
+    print(word,len(word))
+
+
+
+
