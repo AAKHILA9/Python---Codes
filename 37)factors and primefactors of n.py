43ff0350d0943ac6a016183ed6ac24db5f6fdb88
A.Akhila <anemoniakhila123456@gmail.com>
2025-07-30 13:41:27 +0530

    Add files via upload

---
+++ 37)factors and primefactors of n.py
@@ -0,0 +1,17 @@
+n=28
+factors=[]
+primefactors=[]
+
+for i in range (2,n+1):
+    if n%i==0:
+        factors.append(i)
+print("factors:",factors)
+
+for i in factors:
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        primefactors.append(i)
+print("primefactors:",primefactors)
+
